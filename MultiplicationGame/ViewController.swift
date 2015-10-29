fda39f113a262478aca2492681977e1003b5ad2b
Thomas <harrist9@icloud.com>
2015-10-28 23:14:47 -0400

    Added new files

--- MultiplicationGame/ViewController.swift
+++ MultiplicationGame/ViewController.swift
@@ -7,19 +7,81 @@
 //
 
 import UIKit
+import AdSupport
+import iAd
 
-class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+
+class ViewController: UIViewController, ADBannerViewDelegate {
+//Properties
+    var multiple: Int = 0
+    var addidive: Int = 0
+    var sum: Int = 0
+    
+    
+ //Outlets
+    
+    @IBOutlet weak var playBtn: UIButton!
+    @IBOutlet weak var whatToAddBy: UITextField!
+    @IBOutlet weak var logo: UIImageView!
+    
+    @IBOutlet weak var addLabel: UILabel!
+    @IBOutlet weak var addBtn: UIButton!
+ 
+//Actions
+    
+   
+    
+    @IBAction func playBtnPressed(sender: AnyObject) {
+        
+        if whatToAddBy.text != nil && whatToAddBy.text != "" {
+        
+        playBtn.hidden = true
+        whatToAddBy.hidden = true
+        logo.hidden = true
+            
+        addLabel.hidden = false
+        addBtn.hidden = false
+            
+        multiple = Int(whatToAddBy.text!)!
+            self.view.endEditing(true)
+            
+        }
+        
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    
+    @IBAction func add(sender: AnyObject) {
+        
+        sum = addidive + multiple
+        updateLable()
+        addidive = sum
+        
+        if sum >= 100 {
+           
+            restartGame()
+        }
     }
+    
 
+//Functions
+    
+    func updateLable(){
+        addLabel.text = "\(addidive) + \(multiple) = \(sum)"
+    
+    }
+    
+    func restartGame(){
+        
+        playBtn.hidden = false
+        whatToAddBy.hidden = false
+        logo.hidden = false
+        
+        addLabel.hidden = true
+        addBtn.hidden = true
+        whatToAddBy.text = ""
 
+    }
+    
 }
 
